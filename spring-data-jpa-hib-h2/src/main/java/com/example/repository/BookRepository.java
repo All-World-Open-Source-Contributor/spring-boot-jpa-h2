d19f12ef43f0e388e350ead0825bce128a28645d
Subrat Das <subrat.das@bt.com>
2020-02-19 13:58:41 +0530

    spring jpa h2 example

---
+++ spring-data-jpa-hib-h2/src/main/java/com/example/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.model.Book;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    List<Book> findByName(String name);
+
+}
